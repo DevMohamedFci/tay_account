1364a32c66c6f165a69fe63f6a78c19cd6e7a04c
DevMohamedFci <41862946+DevMohamedFci@users.noreply.github.com>
2018-09-13 06:12:46 -0700

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "tay_account",
+
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+
+    'description': """
+        this app to provide customer & vendor transaction report
+    """,
+
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '10.0',
+    'license':'AGPL-3',
+    'suppor':'taybahsoft@gmail.com'
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','account_accountant'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
